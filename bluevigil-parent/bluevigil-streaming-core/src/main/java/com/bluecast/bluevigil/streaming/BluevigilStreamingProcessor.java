e35e6b422daa4821348d753f3de14c96ddd7d60f
Mohammed Asif <asif.the.winner@gmail.com>
2018-01-05 18:56:41 +0530

    Updated Comments to include the commandline args

--- bluevigil-parent/bluevigil-streaming-core/src/main/java/com/bluecast/bluevigil/streaming/BluevigilStreamingProcessor.java
+++ bluevigil-parent/bluevigil-streaming-core/src/main/java/com/bluecast/bluevigil/streaming/BluevigilStreamingProcessor.java
@@ -7,10 +7,13 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 /**
  ** 
  * This program can be executed by following command from your local build jar
- * location spark-submit --master yarn --deploy-mode cluster --class
- * com.bluecast.bluevigil.streaming.BluevigilStreamingProcessor
- * spark-realtime-core-jar-with-dependencies.jar localhost:9092 localhost:2181
- * SparkTest
+ * location 
+ * spark-submit --master yarn --deploy-mode cluster --class com.bluecast.bluevigil.streaming.BluevigilStreamingProcessor  
+ * spark-realtime-core-jar-with-dependencies.jar 
+ * Yassar060Blue 
+ * Yassar060BlueOutput 
+ * nn01.itversity.com:6667,nn02.itversity.com:6667,rm01.itversity.com:6667 
+ * nn01.itversity.com:2181,nn02.itversity.com:2181,rm01.itversity.com:2181
  * 
  */
 public class BluevigilStreamingProcessor {
@@ -21,10 +24,10 @@ public class BluevigilStreamingProcessor {
 	private static String ZOOKEEPER_SERVER;
 
 	public static void main(String args[]) {
-		SOURCE_TOPIC = args[0];
-		DEST_TOPIC = args[1];
-		BOOTSTRAP_SERVERS = args[2];
-		ZOOKEEPER_SERVER = args[3];
+		SOURCE_TOPIC = args[0]; // Source topic - Flume ingest logs to this topic
+		DEST_TOPIC = args[1]; // The spark processed data kept in this topic
+		BOOTSTRAP_SERVERS = args[2]; // Bootstrap server details, comma seperated
+		ZOOKEEPER_SERVER = args[3]; // Zookeeper server details, comma seperated
 
 		BluevigilConsumer consumer = new BluevigilConsumer();
 		SparkConf conf = new SparkConf().setAppName("BluevigilStreamingProcessor"); //.setMaster("local[*]");
